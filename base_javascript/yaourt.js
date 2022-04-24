8fea0cad1e6c29530b07f6887d779aca265dcdee
Jonas4884 <hei.jonatana@gmail.com>
2022-04-24 06:07:29 +0300

    Prendre la seconde couleur le plus populaire

--- base_javascript/yaourt.js
+++ base_javascript/yaourt.js
@@ -25,3 +25,13 @@ for (let i = 0; i < stock.length; i++) {
         max = i+1;
     }
 }
+//Alana izay max ahazoana n second max
+color.splice(max,1);
+stock.splice(max,1);
+//angalana n second max
+let secondmax = 0;
+for (let k = 0; k < stock.length; k++) {
+    if (stock[k+1] > stock[k]) {
+        secondmax = k+1;
+    }
+}
