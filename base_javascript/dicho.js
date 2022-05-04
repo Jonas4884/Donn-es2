5dd78fd267b6519a0a0098919eb8335e3a54bb82
Jonas4884 <hei.jonatana@gmail.com>
2022-05-04 06:10:04 +0300

    fix algo feat recherche dichotomique

--- base_javascript/dicho.js
+++ base_javascript/dicho.js
@@ -1,12 +1,18 @@
+//Cette algorithme a pour but de réaliser une recherche dichotomique.C'est méthode qui consiste à rechecher un donnée par les index
+//Ceci est consue comme une des méthodes de recherche les plus optimales
 
+//Début de l'algorithme
 function dicho(tab, search) {
+ //Attributs
 	let begin = 0;
 	let end = tab .length - 1;
+
+//Diviser le tableau en deux tant qu'on ne truve la valeur recherchée et length>1 
 	while (begin != end) {
 		mid =tab.length-1 ;
 		position = begin + (mid / 2);
 		position = Math.floor(position);
-
+//Si un des valeur du tableau correspond aux valeur recherchées,ses conditions retournera cette valeur
 		if (search == tab[position]) {
 			return position;
 		} else if (search > tab [position]) {
@@ -16,9 +22,10 @@ function dicho(tab, search) {
 		}
 	}
 
-  
-
+  //Sinon s'il n'est pas dans ce tableau,il faut retourner -1 comme le principe de recherche d'index
 	return -1;
 }
+
+//Exemple d'entrée/sortie
 var verlan = [9, 3, 2, 5, 9, 2];
 console.log(dicho(verlan, 10));
