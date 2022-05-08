2e0287f8dc63c7093c79cf9fd2581d7bc3c9b1e4
Jonas4884 <hei.jonatana@gmail.com>
2022-05-08 21:57:02 +0300

    Fix ajouter un projet feat WEB & PROG

--- base_javascript/indexOf.js
+++ base_javascript/indexOf.js
@@ -1,7 +1,13 @@
+//Ceci est une algorithme servant à trouver l'index d'une parametre donné sur une INPUT
+
 function index(word,search){
+
+   //Lire toue les valeurs avec un boucle
    for(let i=0;i<word.length;i++){
+      //Si l'element convient,retourne cette interratif(c'est aussi l'index du parametres)
       if(word[i]==search)
       return i  
    }
+   //Sinon,par principe,on retournera -1 si aucune des nombres ne convient au paramètres
    return -1}
 console.log(index('word','a'));
