406bb597cccd086a71d7e916fb754f11aa9402d6
Jonas4884 <hei.jonatana@gmail.com>
2022-05-09 20:49:28 +0300

    Fix projet feat PROG-longestSeries

---
+++ base_javascript/longestSeries.js
@@ -0,0 +1,22 @@
+export function longestSeries(tab){
+    let count=0;
+    let temp=[];
+    if(tab.length==0){
+        return 0;
+    }
+    if(tab==undefined||tab==null)
+    throw ("Invalid Value");
+for (let i=0; i<tab.length; i++){
+    if(tab[i]==tab[i+1]){
+        count++;
+    }
+    
+    else{
+        temp.push(count);
+        count=1;
+    }
+}
+return Math.max(...temp);
+}
+const tab = [10,5,3,3,4,9,9,9,9,9,6];
+console . log (longestSeries(tab));
